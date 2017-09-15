4c95b72df76799df531778c9cc8e24e82e2326b1
NeverMine17 <dannevergame@gmail.com>
2017-09-15 17:29:34 +0400

    Убрал каракули и немного подправил код

--- main.cpp
+++ main.cpp
@@ -1,36 +1,36 @@
 #include "TXLib.h"
-using namespace std;
-//Илья Цюрюпа
+#include <string>
 
 int main()
 {
-    int p;
-    int x =1300;
-    int y =600;
+    std::string ps;
+    int x = 1300;
+    int y = 600;
 
     txCreateWindow(x, y);
-    HDC a  = txLoadImage ("a.bmp");
-    HDC b  = txLoadImage ("b.bmp");
-    HDC c  = txLoadImage ("c.bmp");
-    HDC v  = txLoadImage ("v.bmp");
+    HDC a = txLoadImage("a.bmp");
+    HDC b = txLoadImage("b.bmp");
+    HDC c = txLoadImage("c.bmp");
+    HDC v = txLoadImage("v.bmp");
 
 
 
-    printf("Выберите автомобиль\n1-McLaren P1\n2-Ferrari Enzo\n3-Газель Next\n");
-    scanf("%d", &p);
+    std::cout << "1-McLaren P1\n2-Ferrari Enzo\n3-Gazel Next" << std::endl;
+    std::cin >> ps;
+    int p = std::atoi(ps);
 
     if (p == 1) {
-    txBitBlt  (txDC(), 0, 0, x, y, a, 0, 0);
-    txBitBlt(txDC(), 0, 60, 400, 160, v, 0, 0);
+        txBitBlt(txDC(), 0, 0, x, y, a, 0, 0);
+        txBitBlt(txDC(), 0, 60, 400, 160, v, 0, 0);
     }
     if (p == 2) {
-    txBitBlt  (txDC(), 0, 0, x, y, b, 0, 0);
-    txBitBlt(txDC(), 0, 70, 400, 170, v, 0, 0);
+        txBitBlt(txDC(), 0, 0, x, y, b, 0, 0);
+        txBitBlt(txDC(), 0, 70, 400, 170, v, 0, 0);
     }
 
     if (p == 3) {
-    txBitBlt  (txDC(), 0, 0, x, y, c, 0, 0);
-    txBitBlt  (txDC(), 50, 30, 450, 130, v, 0, 0);
+        txBitBlt(txDC(), 0, 0, x, y, c, 0, 0);
+        txBitBlt(txDC(), 50, 30, 450, 130, v, 0, 0);
     }
 
     txDeleteDC(a);
